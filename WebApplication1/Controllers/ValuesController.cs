f9a71d836d62f0d2e28a5ec3a5b2294f06d120db
Amol Aher <amola@winjit.com>
2019-06-04 17:10:38 +0530

    values controller changes

--- WebApplication1/Controllers/ValuesController.cs
+++ WebApplication1/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace WebApplication1.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "AmoltestValue", DateTime.Now.ToLongDateString().ToString()};
         }
 
         // GET api/values/5
